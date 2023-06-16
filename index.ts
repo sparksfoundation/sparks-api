54d9cea5dac5f5065330d0d76e7278df8e74cfbe
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2023-06-16 15:39:50 -0400

    add channels endpoint and cleanup

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 const fastify = require('fastify')
 const swarmRelay = require('./src/swarm-relay')
-const { initRestChannel } = require('./src/channels')
+const { receiveChannels } = require('./src/channels')
 const server = fastify()
 
 server.register(swarmRelay)
@@ -9,7 +9,7 @@ server.get('/ping', async () => {
     return 'pong\n'
 })
 
-initRestChannel(server).then(() => {
+receiveChannels(server).then(() => {
   server.listen({ port: process.env.PORT || 3400 }, (err: any, address: any) => {
       if (err) {
           console.error(err)
--- src/channels/index.ts
+++ src/channels/index.ts
@@ -1,18 +1,29 @@
 import { FastifyInstance } from "fastify";
-
-const { Spark, Random, Ed25519, Blake3, X25519SalsaPoly, Fastify: FastifyChannel } = require('sparks-sdk');
+const { Spark, Random, Ed25519, Blake3, X25519SalsaPoly, RestAPI } = require('sparks-sdk');
+const simpleSignalServer = require('simple-signal-server');
 
 const channels = new Spark({
     controller: Random,
     signer: Ed25519,
     hasher: Blake3,
     cipher: X25519SalsaPoly,
-    channels: [ FastifyChannel ]
+    channels: [ RestAPI ]
 });
 
-module.exports.initRestChannel = async (server: FastifyInstance) => {
-    await channels.controller.incept()
-    channels.channels.Fastify.receive(({ details, resolve, reject }: { details: any, resolve: any, reject: any }) => {
+channels.controller.incept();
+channels.channels._RestAPI.receive(
+    ({ details, resolve, reject }: { details: any, resolve: any, reject: any }) => {
         resolve();
-    }, { spark: channels, fastify: server })
+    }, { spark: channels }
+);
+
+const receiveChannels = async (server: FastifyInstance) => {
+    server.post('/channels', async (request, reply) => {
+        const payload = request.body
+        const response = await channels.channels._RestAPI.eventHandler(payload);
+        reply.send(response);
+    })
 }
+
+module.exports.receiveChannels = receiveChannels
+
