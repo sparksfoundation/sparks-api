8ba7f97bbfad18e1827ab824a8d5265aebf111c7
Paul Pomerleau <paul.m.pomerleau@gmail.com>
2023-06-24 00:47:37 -0400

    http channel wip

--- src/channels/restAPI.ts
+++ src/channels/restAPI.ts
@@ -7,27 +7,25 @@ const { X25519SalsaPoly } = require('sparks-sdk/ciphers');
 const { RestAPI } = require('sparks-sdk/channels/Http');
 
 const channels = new Spark({
-    controller: Random,
-    signer: Ed25519,
-    hasher: Blake3,
-    cipher: X25519SalsaPoly,
+  controller: Random,
+  signer: Ed25519,
+  hasher: Blake3,
+  cipher: X25519SalsaPoly,
 });
-
 channels.controller.incept();
+
 RestAPI.receive(
-    ({ details, resolve, reject }: { details: any, resolve: any, reject: any }) => {
-        resolve();
-    }, { spark: channels }
+  async ({ details, resolve, reject }: { details: any, resolve: any, reject: any }) => {
+    const channel = await resolve();
+  }, { spark: channels }
 );
 
 const receiveChannels = async (server: FastifyInstance) => {
-    server.post('/restAPI', async (request, reply) => {
-        const payload = request.body
-        const response = await RestAPI.eventHandler(payload);
-        reply.send(response);
-    })
+  server.post('/restAPI', async (request, reply) => {
+    const payload = request.body as any;
+    const response = await RestAPI.requestHandler(payload);
+    reply.send(response || {});
+  })
 }
 
-
 module.exports.receiveChannels = receiveChannels
-
