ed026429c598fb708ddda008ecff82cc19f6d8b4
sparksdev <dev@sparks.dev>
2023-07-27 00:49:31 -0400

    remove samesite, update configs

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,35 @@
-import { createId } from "@paralleldrive/cuid2"
-
-require('dotenv').config()
-const fastify = require('fastify')
-const server = fastify()
-const { enableSwarmRelay } = require('./swarm-relay')
-const { receiveChannels } = require('./channels/restAPI')
-const { credentials } = require('./credentials')
-const { ethereum } = require('./credentials/ethereum')
-const fastifyCookie = require('@fastify/cookie')
-const fastifySession = require('@fastify/session')
-const cors = require('@fastify/cors')
+require('dotenv').config();
+const fastify = require('fastify');
+const server = fastify();
+const { enableSwarmRelay } = require('./swarm-relay');
+const { receiveChannels } = require('./channels/restAPI');
+const { credentials } = require('./credentials');
+const fastifyCookie = require('@fastify/cookie');
+const fastifySession = require('@fastify/session');
+const cors = require('@fastify/cors');
 
 const start = async () => {
+  server.register(fastifyCookie);
+
   await server.register(cors, {
     origin: process.env.IDENTITY_APP_ORIGIN,
     credentials: true
   });
-  server.register(fastifyCookie);
+
   server.register(fastifySession, {
-    cookieName: createId(),
     secret: process.env.SESSION_SECRET,
-    cookie: { secure: !process.env.IDENTITY_APP_ORIGIN?.startsWith('http://localhost') },
+    cookie: { secure: 'auto' },
+    sameSite: false,
     expires: 1800000
   });
 
-  await enableSwarmRelay(server)
-  await receiveChannels(server)
-  await credentials(server)
+  await enableSwarmRelay(server);
+  await receiveChannels(server);
+  await credentials(server);
 
   server.get('/ping', async () => {
     return 'pong\n'
-  })
+  });
 
   server.listen({ port: process.env.PORT || 3400 }, (err: any, address: any) => {
     if (err) {
@@ -38,7 +37,7 @@ const start = async () => {
       process.exit(1)
     }
     console.log(`Server listening at ${address}`)
-  })
+  });
 }
 
 start();
